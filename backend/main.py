72ce35958994871c4289b511a566592b007882d4
Yiğit ERDOĞAN <yigiterdogan023@gmail.com>
2025-12-30 09:33:58 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,4 +19,5 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    
 # A
