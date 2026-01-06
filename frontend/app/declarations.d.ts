4434e6f077f2b716a6e76a182ea3b0219de00ea5
Yigtwxx <yigiterdogan023@gmail.com>
2026-01-06 14:36:52 +0300

    update

--- frontend/app/declarations.d.ts
+++ frontend/app/declarations.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 declare module 'react-syntax-highlighter' {
     export const Prism: any;
     export const Light: any;
@@ -10,3 +11,4 @@ declare module 'react-syntax-highlighter/*' {
     export default content;
     export const vscDarkPlus: any;
 }
+
